20cbca548ea83c810a4ec8799b52d9133c210cbf
kentaro.sasaki <kentaro.sasaki@rakuten.com>
2018-03-29 19:05:19 +0900

    modify to check installed ansible version

--- setup.sh
+++ setup.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [ ${EUID:-${UID}} -ne 0 ]; then
-  echo "WARN: This script must be run as root" 1>&2
-  exit 1
-fi
-
 ANSIBLE_VERSION="2.4.2.0"
 
 yum_makecache_retry() {
@@ -19,23 +14,54 @@ yum_makecache_retry() {
   done
 }
 
-if [ -f /etc/centos-release ]; then
-  # Check distro version
-  if [ -n "$(rpm -q centos-release | grep el7)" ]; then
-    # Check ansible is installed or not
-    if [ -z "$(rpm -qa | grep ansible-$ANSIBLE_VERSION)" ]; then
-      yum clean all
-      rm -rf /var/cache/yum
-      yum_makecache_retry
-      # Install Git and Ansible
-      yum -y install git ansible-$ANSIBLE_VERSION
-    else
-      INSTALLED_ANSIBLE=$(rpm -qa | grep ansible-$ANSIBLE_VERSION)
-      echo "WARN: Ansible rpm: $INSTALLED_ANSIBLE is already installed"
-    fi
-  else
-    echo "WARN: Could not detect supported specific CentOS version: CentOS 7.X"
+if [ ${EUID:-${UID}} -ne 0 ]; then
+  echo "WARN: This script must be run as root." 1>&2
+  exit 1
+fi
+
+# Check Linux distro
+if [ ! -e /etc/centos-release ]; then
+  echo "WARN: Could not detect supported Linux distro: CentOS." 1>&2
+  exit 1
+fi
+
+# Check distro version
+if [ -z "$(rpm -q centos-release | grep el7)" ]; then
+  echo "WARN: Could not detect supported specific CentOS version: CentOS 7.X." 1>&2
+  exit 1
+fi
+
+yum clean all
+rm -rf /var/cache/yum
+yum_makecache_retry
+# Update all packages
+yum -y update
+
+# Check git is installed or not
+if [ -n "$(which git)" ]; then
+  echo "INFO: Git is already installed." 1>&2
+else
+  ## Install Git
+  yum -y install git
+  echo "INFO: Git has been successfully installed."
+fi
+
+# Check ansible is installed or not
+if [ -n "$(which ansible-playbook)" ]; then
+  INSTALLED_ANSIBLE_VERSION=$(ansible --version | awk 'NR==1' | awk -F' ' '{print $2}')
+  echo "INFO: Ansible $INSTALLED_ANSIBLE_VERSION is already installed." 1>&2
+  echo ""
+  echo "INFO: Installed Ansible version:          $INSTALLED_ANSIBLE_VERSION" 1>&2
+  echo "INFO: Target Ansible version we expected: $ANSIBLE_VERSION" 1>&2
+  echo ""
+  if [ $INSTALLED_ANSIBLE_VERSION != $ANSIBLE_VERSION ]; then
+    echo "WARN: Installed Ansible version does not equal to $ANSIBLE_VERSION," 1>&2
+    echo "WARN: please uninstall Ansible at once and execute this script again." 1>&2
+    exit 1
   fi
 else
-  echo "WARN: Could not detect supported Linux distro: CentOS"
+  # Install Ansible
+  yum -y install ansible-$ANSIBLE_VERSION
+  echo "Ansible has been successfully installed."
 fi
+
